5ad09191c4f3a9cafd8fa9e6f4fcc5c8625083b4
kanoutef163 <kanoutef163@gmail.com>
2026-05-06 10:30:57 +0100

    Initial commit: SenAgricultureS2

---
+++ FrontSenAgriculture/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrontSenAgriculture
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ServiceSenAgriculture.Service1Client service = new ServiceSenAgriculture.Service1Client();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String a = service.GetData(1);
+            MessageBox.Show(a);
+        }
+    }
+}
